2b28ff4cd215a54cd64135578d96a6b6da535647
s.makarov <smakarov@avo.uz>
2024-12-06 19:30:35 +0300

    lesson 6

--- src/tms/lesson5/MainApp.java
+++ src/tms/lesson5/MainApp.java
@@ -3,7 +3,7 @@ package tms.lesson5;
 public class MainApp {
 
     public static void main(String[] args) {
-        System.out.println("Lesson 5");
+        System.out.println("Lesson 5 color");
     }
 
 }
