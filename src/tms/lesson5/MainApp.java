91149dce9e9f802e42cb327466aa1b3b34285068
s.makarov <smakarov@avo.uz>
2024-12-06 19:49:54 +0300

    lesson 6 full !

--- src/tms/lesson5/MainApp.java
+++ src/tms/lesson5/MainApp.java
@@ -4,6 +4,10 @@ public class MainApp {
 
     public static void main(String[] args) {
         System.out.println("Lesson 5 color");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World !!!");
+        }
     }
 
 }
