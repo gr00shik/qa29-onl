c6bf28b885a7c5da1477521dc6aab002b07972ec
s.makarov <smakarov@avo.uz>
2024-12-11 08:55:54 +0300

    lesson 5 classes

---
+++ src/tms/lesson6/MathService.java
@@ -0,0 +1,17 @@
+package tms.lesson6;
+
+public class MathService {
+
+    public int sum(int v1, int v2) {
+        return v1 + v2;
+    }
+
+    public int minus(int v1, int v2) {
+        return v1 - v2;
+    }
+
+    public void multiply(int v1, int v2) {
+        System.out.println(v1 * v2);
+    }
+
+}
